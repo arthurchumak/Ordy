4e9d73839f7e0c98fc6353aab0b0972e7153020b
Arthur Chumak <arthurchumak@gmail.com>
2017-05-13 13:04:45 +0300

    fix(settings): add heroku settings

--- Ordy/settings.py
+++ Ordy/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 
 # Application definition
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
